639f1444d57144cdb796cfc0b0e5ca8ff63a6423
yanghongwei <yanghongwei@boo.chat>
2019-05-24 18:05:39 +0800

    添加一些通用的工具函数和filter

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import './styles/style'
 
+// Global filters
+import * as filters from '@/utils/filters'
+
+// Regist filters
+Object.keys(filters).forEach(k => Vue.filter(k, filters[k]))
+
 Vue.config.productionTip = false
 
 new Vue({
